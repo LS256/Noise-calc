cf1ad052abce9476534958f047613262861ded75
LS256 <lstaszew@gmail.com>
2017-09-19 14:08:24 +0200

    Add files via upload

---
+++ halas.java
@@ -0,0 +1,7 @@
+/*
+* nominal frequnecy of middle octave band and Ground Absorption factors for following frequencies
+*  genericOctaveValues = {63Hz, 125Hz, 250Hz, 500Hz, 1000Hz, 2000Hz, 4000Hz, 8000Hz};
+* groundAbsorptionFactor = {63Hz, 125Hz, 250Hz, 500Hz, 1000Hz, 2000Hz, 4000Hz, 8000Hz};
+*/
+private double[] genericOctaveValues = {31.6, 38.6, 42, 44.6, 44.4, 41.5, 36.7, 27.2};
+private double[] groundAbsorptionFactor = {0.1, 0.4, 1, 1.9, 3.7, 9.7, 32.8, 117};
