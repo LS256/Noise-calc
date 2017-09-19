1eb89f32e920808e81eac70261cad2e29862cf04
LS256 <lstaszew@gmail.com>
2017-09-19 14:10:34 +0200

    Update halas.java

--- halas.java
+++ halas.java
@@ -5,3 +5,4 @@
 */
 private double[] genericOctaveValues = {31.6, 38.6, 42, 44.6, 44.4, 41.5, 36.7, 27.2};
 private double[] groundAbsorptionFactor = {0.1, 0.4, 1, 1.9, 3.7, 9.7, 32.8, 117};
+private double[] distance
